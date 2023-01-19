39b7ec8d86b61634d0d6a927b8e4bde84ab00221
Harshal Bhakta <harshal.c.bhakta@gmail.com>
2023-01-19 13:25:41 +0530

    Using a Column

--- app/src/main/java/com/harshalbhakta/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/harshalbhakta/jetpackcomposetutorial/MainActivity.kt
@@ -3,6 +3,7 @@ package com.harshalbhakta.jetpackcomposetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -25,8 +26,10 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(message: Message) {
-    Text(message.author)
-    Text(message.body)
+    Column {
+        Text(text = message.author)
+        Text(text = message.body)
+    }
 }
 
 @Preview
