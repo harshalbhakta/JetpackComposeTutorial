f6567e226bede0d0f8162880f8637fbe0be3db55
Harshal Bhakta <harshal.c.bhakta@gmail.com>
2023-01-19 13:08:52 +0530

    Add a text element

--- app/src/main/java/com/harshalbhakta/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/harshalbhakta/jetpackcomposetutorial/MainActivity.kt
@@ -16,28 +16,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeTutorialTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            Text("Hello World")
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JetpackComposeTutorialTheme {
-        Greeting("Android")
     }
 }
