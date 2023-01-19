d1412f2816f677aff784acd688979f9e14b8ba72
Harshal Bhakta <harshal.c.bhakta@gmail.com>
2023-01-19 13:36:09 +0530

    Add an image element

--- app/src/main/java/com/harshalbhakta/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/harshalbhakta/jetpackcomposetutorial/MainActivity.kt
@@ -3,13 +3,16 @@ package com.harshalbhakta.jetpackcomposetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.harshalbhakta.jetpackcomposetutorial.ui.theme.JetpackComposeTutorialTheme
 
@@ -26,9 +29,15 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(message: Message) {
-    Column {
-        Text(text = message.author)
-        Text(text = message.body)
+    Row {
+        Image(
+            painter = painterResource(R.drawable.harshal_cital_profile),
+            contentDescription = "Contact profile picture",
+        )
+        Column {
+            Text(text = message.author)
+            Text(text = message.body)
+        }
     }
 }
 
