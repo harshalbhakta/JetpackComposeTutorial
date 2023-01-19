a6043c026dd66370ce4e14eb1db9918d42b37e45
Harshal Bhakta <harshal.c.bhakta@gmail.com>
2023-01-19 13:16:01 +0530

    Preview your function in Android Studio

--- app/src/main/java/com/harshalbhakta/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/harshalbhakta/jetpackcomposetutorial/MainActivity.kt
@@ -25,3 +25,9 @@ class MainActivity : ComponentActivity() {
 fun MessageCard(name: String) {
     Text("Hello $name!")
 }
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard("Android")
+}
